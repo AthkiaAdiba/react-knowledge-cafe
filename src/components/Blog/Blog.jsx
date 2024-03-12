45642d736f981fc665e14a52cbae537a31f3bbca
thkiaAdiba <athkiaadiba@gmail.com>
2024-03-12 23:48:15 +0600

    added blogs carts

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,10 +1,29 @@
 import PropTypes from 'prop-types';
 
-const Blog = ({blog}) => {
-    console.log(blog);
+const Blog = ({ blog }) => {
+    // console.log(blog);
+    const { title, cover, reading_time, author_name, author_img, posted_date, hashtags } = blog;
     return (
         <div>
-            
+            <img src={cover} alt={`cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex gap-3'>
+                    <img className='w-16 h-16 rounded-full bg-cover' src={author_img} alt="" />
+                    <div>
+                        <h3 className='text-2xl'>{author_name}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
